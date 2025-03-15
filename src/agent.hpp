4747d6e063f0669ba293b2f75a61d04ace59e23b
William L. Thomson Jr. <w@wltjr.com>
2025-03-14 21:40:39 -0400

    src/agent.hpp: Initial GRAPE Agent class header, maybe others?

---
+++ src/agent.hpp
@@ -0,0 +1,42 @@
+#ifndef AGENT_HPP
+#define AGENT_HPP
+
+#include <tuple>
+#include <vector>
+
+class Agent
+{
+    public:
+        /**
+         * @brief Construct a new Agent object, empty/unused
+         */
+        Agent();
+
+        /**
+         * @brief Destroy the Agent object, empty/unused
+         */
+        virtual ~Agent();
+
+    private:
+        bool satisfied;
+        int partition;  // Π
+        int iterations; // r
+        int seed; // s [0,1] uniform-random variable
+
+        /**
+         * @brief Decentralized decision-making algorithm
+         */
+        void decision();
+
+        /**
+         * @brief Distributed Mutex Subroutine
+         * 
+         * @param msgs a vector of tuples, (iterations, seed, partition, satisfied)
+         * 
+         * @return a tuple (iterations, seed, partition, satisfied)
+         */
+        std::tuple<int, int, int, bool> decision_mutex(
+            std::vector<std::tuple<int, int, int, bool>> msgs
+        );
+};
+#endif
