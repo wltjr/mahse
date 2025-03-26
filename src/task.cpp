58225b7602a55db498b46051f386afd44edbaea3
William L. Thomson Jr. <w@wltjr.com>
2025-03-25 20:18:19 -0400

    src/task.cpp: Added reward modifier to constructor and getter

--- src/task.cpp
+++ src/task.cpp
@@ -4,5 +4,26 @@ Task::Task() = default;
 
 Task::~Task() = default;
 
-Task::Task(int id, Point coords, int reward) :
-    id(id), coords(coords), reward(reward) {};
+Task::Task(int id, Point coords, int reward, int modifier) :
+    id(id), coords(coords), reward(reward), modifier(modifier) {};
+
+Point Task::get_coords()
+{
+    return(coords);
+}
+
+int Task::get_id()
+{
+    return(id);
+}
+
+int Task::get_modifier()
+{
+    return(modifier);
+}
+
+int Task::get_reward()
+{
+    return(reward);
+}
+
