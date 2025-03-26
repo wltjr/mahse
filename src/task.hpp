5a71fb505fc4c7910bf85d5079d5a4880ef54840
William L. Thomson Jr. <w@wltjr.com>
2025-03-25 20:10:38 -0400

    src/task.hpp: Added reward modifier variable, getter, and comments

--- src/task.hpp
+++ src/task.hpp
@@ -16,9 +16,38 @@ class Task
          * 
          * @param id the task id
          * @param coords the task coordinates (x,y)
-         * @param reward the task reward
+         * @param reward the task reward, max or min for peaked or submodular
+         * @param modifier the reward modifier, n_j^d for peaked or ε_j for submodular
          */
-        Task(int id, Point coords, int reward);
+        Task(int id, Point coords, int reward, int modifier);
+
+        /**
+         * @brief Coords getter
+         *
+         * @return the tasks coordinates (x,y)
+         */
+        Point get_coords();
+
+        /**
+         * @brief Id getter
+         *
+         * @return the tasks id
+         */
+        int get_id();
+
+        /**
+         * @brief Modifier getter
+         *
+         * @return the tasks reward modifier value
+         */
+        int get_modifier();
+
+        /**
+         * @brief Reward getter
+         *
+         * @return the tasks reward value
+         */
+        int get_reward();
 
         /**
          * @brief Destroy the Task object, empty/unused
@@ -28,7 +57,8 @@ class Task
     private:
         int id;
         Point coords;
-        int reward;
+        int reward; // max or min for peaked or submodular
+        int modifier; // either n_j^d for peaked or ε_j for submodular
 };
 
 #endif
