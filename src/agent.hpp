df3cba07e1cd3863161b1134949548ed5cd219c9
William L. Thomson Jr. <w@wltjr.com>
2025-03-27 14:21:35 -0400

    src/agent.hpp: Added rewards enum and utility wrapper method
    - Enum will control which reward heuristic is used in utility wrapper

--- src/agent.hpp
+++ src/agent.hpp
@@ -9,6 +9,11 @@
 class Agent
 {
     public:
+        enum rewards {
+            peaked,
+            submodular
+        };
+
         /**
          * @brief Construct a new Agent object, empty/unused
          */
@@ -20,8 +25,9 @@ class Agent
          * @param id the task id
          * @param coords the task coordinates (x,y)
          * @param tasks vector of tasks
+         * @param reward the reward heuristic to use
          */
-        Agent(int id, Point coords, std::vector<Task> &tasks);
+        Agent(int id, Point coords, std::vector<Task> &tasks, rewards reward);
 
         /**
          * @brief Euclidean distance between task and agent
@@ -39,6 +45,13 @@ class Agent
         int reward_submodular(int task);
 
         /**
+         * @brief Utility function
+         *
+         * @param task the task index number, e.g. the j in (t_j, S_j)
+         */
+        int utility(int task);
+
+        /**
          * @brief Destroy the Agent object, empty/unused
          */
         virtual ~Agent();
@@ -51,6 +64,7 @@ class Agent
         std::vector<Task> tasks; // T
         int iterations; // r
         float seed; // s [0,1] uniform-random variable
+        int reward;
 
         /**
          * @brief Decentralized decision-making algorithm
