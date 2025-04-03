eddba426a4050b59a33b5d8b484d983fa3ca28c9
William L. Thomson Jr. <w@wltjr.com>
2025-04-03 13:44:40 -0400

    src/task.cpp: Implemented pack/unpack methods for MPI_{pack,unpack}

--- src/task.cpp
+++ src/task.cpp
@@ -27,3 +27,22 @@ int Task::get_reward()
     return(reward);
 }
 
+#include <iostream>
+
+void Task::pack(char *buffer, int size, int &position, MPI_Comm comm)
+{
+    MPI_Pack(&id, 1, MPI_INT, buffer, size, &position, comm);
+    MPI_Pack(&coords.x, 1, MPI_INT, buffer, size, &position, comm);
+    MPI_Pack(&coords.y, 1, MPI_INT, buffer, size, &position, comm);
+    MPI_Pack(&reward, 1, MPI_INT, buffer, size, &position, comm);
+    MPI_Pack(&modifier, 1, MPI_INT, buffer, size, &position, comm);
+}
+
+void Task::unpack(char *buffer, int size, int &position, MPI_Comm comm)
+{
+    MPI_Unpack(buffer, size, &position, &id, 1, MPI_INT, comm);
+    MPI_Unpack(buffer, size, &position, &coords.x, 1, MPI_INT, comm);
+    MPI_Unpack(buffer, size, &position, &coords.y, 1, MPI_INT, comm);
+    MPI_Unpack(buffer, size, &position, &reward, 1, MPI_INT, comm);
+    MPI_Unpack(buffer, size, &position, &modifier, 1, MPI_INT, comm);
+}
