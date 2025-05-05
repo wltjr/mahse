6f338dfdaa363a6936ad60ad6d9c0e44e4208fd0
William L. Thomson Jr. <w@wltjr.com>
2025-05-05 11:04:52 -0400

    src/main.cpp: Fix code smell remove redundant parentheses

--- src/main.cpp
+++ src/main.cpp
@@ -64,7 +64,7 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
         default:
             return ARGP_ERR_UNKNOWN;
     }
-    return(0);
+    return 0;
 }
 
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
