c8972fd8cb07a71fb50bdbfacf37a04864da8973
William L. Thomson Jr. <w@wltjr.com>
2025-05-06 10:35:32 -0400

    src/main.cpp: Fix code smell replace NULL with nullptr

--- src/main.cpp
+++ src/main.cpp
@@ -103,7 +103,7 @@ int main(int argc, char* argv[])
     std::uniform_real_distribution<> urd1(0, args.dim);
 
     // Initialize MPI
-    MPI_Init(NULL, NULL);
+    MPI_Init(nullptr, nullptr);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
